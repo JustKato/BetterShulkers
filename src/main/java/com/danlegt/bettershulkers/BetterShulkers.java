ec589cdde10bddc4fa319dc8ce0a19b78c80f234
Daniel Legt <contact@danlegt.com>
2023-08-29 02:12:12 +0300

    + Metrics

--- src/main/java/com/danlegt/bettershulkers/BetterShulkers.java
+++ src/main/java/com/danlegt/bettershulkers/BetterShulkers.java
@@ -1,15 +1,17 @@
 package com.danlegt.bettershulkers;
 
 import com.danlegt.bettershulkers.Events.ShulkerDropEvent;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BetterShulkers extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
         // Register the drop event
         getServer().getPluginManager().registerEvents(new ShulkerDropEvent(), this);
+        // Init bStats
+        Metrics metrics = new Metrics(this, 19672);
     }
 
 }
