8fac2ed8a0041608cb42b7a74e18577f3dd3c91e
Max Dizendorf <max.maxik14@gmail.com>
2023-08-30 01:19:14 +0200

    Added Fix:Preventing player from placing and dropping the same shulker at the same time Added Fix: Preventing Player from dropping the shulker or moving it in the inventory while its open. So changes can be applied after inventoryclose

--- src/main/java/com/danlegt/bettershulkers/BetterShulkers.java
+++ src/main/java/com/danlegt/bettershulkers/BetterShulkers.java
@@ -6,12 +6,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BetterShulkers extends JavaPlugin {
 
+    private static BetterShulkers INSTANCE;
+
     @Override
     public void onEnable() {
         // Register the drop event
+        INSTANCE = this;
         getServer().getPluginManager().registerEvents(new ShulkerDropEvent(), this);
         // Init bStats
         Metrics metrics = new Metrics(this, 19672);
     }
 
+    public static BetterShulkers getINSTANCE() {
+        return INSTANCE;
+    }
 }
